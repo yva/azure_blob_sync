8cca675b5cccb5672d6b2f48de3a5ed85f796605
Danil Demidov <danil@MacBook-Pro.local>
2020-06-15 16:26:45 +0300

    add debug

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,5 @@ if [ -z "$INPUT_SOURCE" ]; then
   echo "source directory is not set. Quitting."
   exit 1
 fi
-az storage blob sync --source ${INPUT_SOURCE} -c '$web' --connection-string "${INPUT_CONNECTION_STRING}"
+#az storage blob sync --source ${INPUT_SOURCE} -c '$web' --connection-string "${INPUT_CONNECTION_STRING}"
+az storage blob sync --source ${INPUT_SOURCE} -c '$web' --connection-string "${INPUT_CONNECTION_STRING}" --debug
